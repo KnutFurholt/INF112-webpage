c52dba246e773b721bde67a0b4fb555da1a9ca1b
KnutFurholt <122993168+KnutFurholt@users.noreply.github.com>
2024-06-06 10:31:27 +0000

    testing igjen igjen

---
+++ RunJavServlet.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/runjar")
+public class RunJarServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Process process = Runtime.getRuntime().exec("java -jar path/to/yourfile.jar");
+            process.waitFor();
+            response.getWriter().write("JAR file executed successfully.");
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.getWriter().write("Error executing JAR file.");
+        }
+    }
+}
